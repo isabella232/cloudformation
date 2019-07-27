470c46a2a981a318295d2810e6636f6ec1d4c906
moody <moody.salem@gmail.com>
2019-07-27 04:38:29 -0400

    makerdao cdp bucket

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+STACK_NAME=$1
+
 aws cloudformation update-stack --region us-east-1 \
-    --stack-name "${1}" \
+    --stack-name "${STACK_NAME}" \
     --template-body "file://templates/site.template.json" \
-    --parameters "file://parameters/${1}.parameters.json" \
+    --parameters "file://parameters/${STACK_NAME}.parameters.json" \
     --capabilities CAPABILITY_IAM
